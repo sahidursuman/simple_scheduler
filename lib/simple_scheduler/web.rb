e8b531a7828d4d3fbef967b59648fd48ad1c993e
Brian Pattison <brian@brianpattison.com>
2017-05-26 16:21:48 -0500

    Remove code for versions of Sidekiq not supported

--- lib/simple_scheduler/web.rb
+++ lib/simple_scheduler/web.rb
@@ -2,11 +2,5 @@ require "simple_scheduler/web_extension"
 
 if defined?(Sidekiq::Web)
   Sidekiq::Web.register SimpleScheduler::WebExtension
-
-  if Sidekiq::Web.tabs.is_a?(Array)
-    # For sidekiq < 2.5
-    Sidekiq::Web.tabs << "simple_scheduler"
-  else
-    Sidekiq::Web.tabs["Simple Scheduler"] = "simple_scheduler"
-  end
+  Sidekiq::Web.tabs["Simple Scheduler"] = "simple_scheduler"
 end
